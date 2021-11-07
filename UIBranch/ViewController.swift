4483640b6bcb85cb286b4b0fba2b3ac1f85df8f8
Ipsky <yap@usc.edu>
2021-11-06 20:31:58 -0700

    misc

--- UIBranch/ViewController.swift
+++ UIBranch/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let table = MainTable()
+    
+    required init?(coder: NSCoder) {
+        super.init(nibName: nil, bundle: nil)
+        adopt(table)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +24,22 @@ class ViewController: UIViewController {
 
 }
 
+final class MainTable: UITableViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("No.")
+    }
+}
+
+extension UIViewController {
+    /// Convenience method for adding a subview to the view hierarchy.
+    func adopt(_ child: UIViewController) {
+        addChild(child)
+        child.view.frame = view.frame
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+}
