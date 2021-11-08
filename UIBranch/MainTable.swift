39c267a9b9db36c9db8f7d9ad11d42bb3dd212eb
Ipsky <yap@usc.edu>
2021-11-07 17:59:36 -0800

    basic layout

--- UIBranch/MainTable.swift
+++ UIBranch/MainTable.swift
@@ -31,6 +31,9 @@ final class MainTable: UITableViewController {
             UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped)),
             UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(addTapped2)),
         ]
+        
+        /// Enable self sizing table view cells.
+        tableView.estimatedRowHeight = 100
     }
     
     @objc
@@ -66,13 +69,19 @@ extension MainTable {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Cell.reuseID, for: indexPath) as! Cell
         let tweet = discussions[indexPath.section].relevantTweets(followingUserIDs: followingIDs)[indexPath.row]
-        cell.textLabel?.text = tweet.text
+        let author = realm.user(id: tweet.authorID)!
+        cell.configure(tweet: tweet, author: author)
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return discussions[section].id
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        /// Enable self sizing table view cells.
+        UITableView.automaticDimension
     }
 }
 
@@ -81,13 +90,79 @@ final class TweetCell: UITableViewCell {
     public static let reuseID = "DiscussionCell"
     override var reuseIdentifier: String? { Self.reuseID }
     
+    /// Component Views
+    let stackView = UIStackView()
+    let userStackView = UIStackView()
+    let handleLabel = UILabel()
+    let tweetLabel = UILabel()
+    // TODO: add profile image
+    // TODO: add retweet marker
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        
+        /// Configure Main Stack View
+        contentView.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+        ])
+
+        /// Configure User Stack View
+        stackView.addArrangedSubview(userStackView)
+        userStackView.axis = .horizontal
+        userStackView.alignment = .firstBaseline
+        userStackView.addArrangedSubview(handleLabel)
+
+        // stackView.addArrangedSubview(handleLabel)
+        stackView.addArrangedSubview(tweetLabel)
+
+        /// Configure Label
+        handleLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        tweetLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        handleLabel.adjustsFontForContentSizeCategory = true
+        tweetLabel.adjustsFontForContentSizeCategory = true
+
+        /// Allow tweet to wrap across lines.
+        tweetLabel.lineBreakMode = .byWordWrapping
+        tweetLabel.numberOfLines = 0 /// Yes, really.
+    }
+
+    public func configure(tweet: Tweet, author: User) {
+        handleLabel.text = author.screen_name
+        tweetLabel.text = tweet.text
     }
     
     required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+final class UserView: UIStackView {
+    
+    public let nameLabel = UILabel()
+    public let handleLabel = UILabel()
+    
+    init() {
+        super.init(frame: .zero)
+        axis = .horizontal
+        alignment = .firstBaseline
+
+        addArrangedSubview(nameLabel)
+        addArrangedSubview(handleLabel)
+
+        nameLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        nameLabel.adjustsFontForContentSizeCategory = true
+        handleLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        handleLabel.adjustsFontForContentSizeCategory = true   
+    }
+
+    required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
