e6e8fef9a5a55210c30c94b3c9f86cfcb14a20e8
Ipsky <yap@usc.edu>
2021-11-13 15:56:24 -0800

    Revert "use symbol config"
    This reverts commit 9368450a8d53d90541fc85e66e9ba8a82ef7b539.

--- UIBranch/TweetCell/MetricButtons.swift
+++ UIBranch/TweetCell/MetricButtons.swift
@@ -9,10 +9,10 @@ import UIKit
 import Twig
 
 final class MetricsView: UIStackView {
-    let replyButton = makeButton(symbolName: "arrowshape.turn.up.left.fill")
-    let retweetButton = makeButton(symbolName: RetweetSymbol.name, symbolConfig: RetweetSymbol.config)
-    let likeButton = makeButton(symbolName: "heart.fill")
-    let timestampButton = makeButton(symbolName: "clock")
+    let replyButton = ReplyButton()
+    let retweetButton = RetweetButton()
+    let likeButton = LikeButton()
+    let timestampLabel = UILabel()
 
     fileprivate let _spacing: CGFloat = 5
     
@@ -29,65 +29,110 @@ final class MetricsView: UIStackView {
         addArrangedSubview(retweetButton)
         addArrangedSubview(likeButton)
         addArrangedSubview(UIView())
-        addArrangedSubview(timestampButton)
-        
-        /// Configure Action.
-        likeButton.addTarget(self, action: #selector(onTapLike), for: .touchUpInside)
-        replyButton.addTarget(self, action: #selector(onTapReply), for: .touchUpInside)
-        retweetButton.addTarget(self, action: #selector(onTapRetweet), for: .touchUpInside)
+        addArrangedSubview(timestampLabel)
+
+        timestampLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        timestampLabel.textColor = .secondaryLabel
+        timestampLabel.adjustsFontForContentSizeCategory = true
     }
 
     func configure(_ tweet: Tweet) {
-        setTitle(button: replyButton, count: tweet.metrics.reply_count)
-        setTitle(button: retweetButton, count: tweet.metrics.retweet_count)
-        setTitle(button: likeButton, count: tweet.metrics.like_count)
-        timestampButton.setTitle(approximateTimeSince(tweet.createdAt), for: .normal)
-    }
-    
-    /// Hide metrics with 0 count.
-    func setTitle(button: UIButton, count: Int) -> Void {
-        if count > 0 {
-            button.setTitle("\(count)", for: .normal)
-        } else {
-            button.setTitle("", for: .normal)
-        }
+        replyButton.configure(tweet)
+        retweetButton.configure(tweet)
+        likeButton.configure(tweet)
+        timestampLabel.text = approximateTimeSince(tweet.createdAt)
     }
 
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+class LabelledButton: UIButton {
+    
+    init(symbolName: String, symbolConfig: UIImage.SymbolConfiguration? = nil) {
+        super.init(frame: .zero)
+        setImage(UIImage(systemName: symbolName), for: .normal)
+        var config = UIImage.SymbolConfiguration.init(paletteColors: [.secondaryLabel])
+        if let other = symbolConfig {
+            config = config.applying(other)
+        }
+        setPreferredSymbolConfiguration(config, forImageIn: .normal)
+        setTitleColor(.secondaryLabel, for: .normal)
+        addTarget(self, action: #selector(onTap), for: .touchUpInside)
+    }
     
     @objc
-    private func onTapLike() -> Void {
-        Swift.debugPrint("Like Not Implemented!")
-    }
-
-    @objc
-    private func onTapRetweet() -> Void {
-        Swift.debugPrint("Retweet Not Implemented!")
-    }
-
-    @objc
-    private func onTapReply() -> Void {
-        Swift.debugPrint("Reply Not Implemented!")
-    }
-
-    /// Factory Method allows us to use convenience initializer for UIButton.
-    /// `configuration` initializer enables Dynamic Type in a way I couldn't figure out.
-    static func makeButton(symbolName: String?, symbolConfig: UIImage.SymbolConfiguration? = nil) -> UIButton {
-        var config = UIButton.Configuration.plain()
-        config.buttonSize = .mini
-        config.baseForegroundColor = .secondaryLabel
-        let button = UIButton(configuration: config, primaryAction: nil)
-        
-        /// Configure Image.
-        if let symbolName = symbolName {
-            button.setImage(UIImage(systemName: symbolName), for: .normal)
+    func onTap() -> Void { /* does nothing */}
+    
+    /// Hide metrics with 0 count.
+    func setTitle(_ count: Int) -> Void {
+        if count > 0 {
+            setTitle("\(count)", for: .normal)
+        } else {
+            setTitle("", for: .normal)
         }
-        if let symbolConfig = symbolConfig {
-            button.setPreferredSymbolConfiguration(symbolConfig, forImageIn: .normal)
-        }
-        
-        return button
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
+
+final class ReplyButton: LabelledButton {
+    init() {
+        super.init(symbolName: "arrowshape.turn.up.left.fill")
+    }
+    
+    @objc
+    override func onTap() -> Void {
+        print("Reply Not Implemented!")
+    }
+
+    func configure(_ tweet: Tweet) {
+        super.setTitle(tweet.metrics.reply_count)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+final class RetweetButton: LabelledButton {
+    init() {
+        super.init(symbolName: RetweetSymbol.name, symbolConfig: RetweetSymbol.config)
+    }
+    
+    @objc
+    override func onTap() -> Void {
+        print("Retweet Not Implemented!")
+    }
+
+    func configure(_ tweet: Tweet) {
+        super.setTitle(tweet.metrics.retweet_count)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+final class LikeButton: LabelledButton {
+    init() {
+        super.init(symbolName: "heart.fill")
+    }
+    
+    @objc
+    override func onTap() -> Void {
+        print("Like Not Implemented!")
+    }
+
+    func configure(_ tweet: Tweet) {
+        super.setTitle(tweet.metrics.like_count)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
