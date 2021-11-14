087ae83ad3a314f6decff55e9cf7825fee6891a7
Ipsky <yap@usc.edu>
2021-11-14 12:29:05 -0800

    declare new class

---
+++ UIBranch/CardCell/CardCell.swift
@@ -0,0 +1,74 @@
+//
+//  CardCell.swift
+//  UIBranch
+//
+//  Created by Secret Asian Man Dev on 14/11/21.
+//
+
+import UIKit
+import RealmSwift
+import Twig
+
+final class CardCell: UITableViewCell {
+    
+    public static let reuseID = "CardCell"
+    override var reuseIdentifier: String? { Self.reuseID }
+    
+    /// Component Views
+    let stackView = UIStackView()
+    let replyView = ReplyView()
+    let userView = UserView()
+    let tweetLabel = UILabel()
+    let retweetView = RetweetView()
+    let metricsView = MetricsView()
+    // TODO: add profile image
+    // TODO: add retweet marker
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        /// Configure Main Stack View
+        contentView.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+        ])
+
+        stackView.addArrangedSubview(replyView)
+        stackView.addArrangedSubview(userView)
+        stackView.addArrangedSubview(tweetLabel)
+        stackView.addArrangedSubview(retweetView)
+        stackView.addArrangedSubview(metricsView)
+        
+        /// Manually constrain to full width.
+        NSLayoutConstraint.activate([
+            metricsView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            metricsView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+        ])
+
+        /// Configure Label
+        tweetLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        tweetLabel.adjustsFontForContentSizeCategory = true
+
+        /// Allow tweet to wrap across lines.
+        tweetLabel.lineBreakMode = .byWordWrapping
+        tweetLabel.numberOfLines = 0 /// Yes, really.
+    }
+
+    public func configure(tweet: Tweet, author: User, realm: Realm) {
+        userView.configure(user: author, timestamp: tweet.createdAt)
+        tweetLabel.text = tweet.text
+        replyView.configure(tweet: tweet, realm: realm)
+        retweetView.configure(tweet: tweet, realm: realm)
+        metricsView.configure(tweet)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
