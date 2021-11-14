3bf375c32f63c975e38ea6ea9a2bc04aad222c74
Ipsky <yap@usc.edu>
2021-11-14 10:08:27 -0800

    remove old functions

--- UIBranch/MainTable.swift
+++ UIBranch/MainTable.swift
@@ -215,37 +215,3 @@ final class Fetcher: NSObject, UITableViewDataSourcePrefetching {
         }
     }
 }
-
-func fetchOld(airport: Airport, credentials: OAuthCredentials) async -> Void {
-    let limitID = UserDefaults.groupSuite.maxID
-    guard let rawTweets = try? await timeline(credentials: credentials, sinceID: nil, maxID: limitID) else {
-        Swift.debugPrint("Failed to fetch timeline!")
-        return
-    }
-    if rawTweets.isEmpty {
-        NetLog.log(items: "No new tweets found!")
-    }
-    let ids = rawTweets.map{"\($0.id)"}
-    airport.enqueue(ids)
-    
-    /// Update boundaries.
-    let newMaxID = min(rawTweets.map(\.id).min(), Int64?(limitID))
-    UserDefaults.groupSuite.maxID = newMaxID.string
-    Swift.debugPrint("newMaxID \(newMaxID ?? 0), previously \(limitID ?? "")")
-    Swift.debugPrint(rawTweets.map(\.id))
-}
-
-func fetchNew(airport: Airport, credentials: OAuthCredentials) async -> Void {
-    let limitID = UserDefaults.groupSuite.sinceID
-    guard let rawTweets = try? await timeline(credentials: credentials, sinceID: limitID, maxID: nil) else {
-        Swift.debugPrint("Failed to fetch timeline!")
-        return
-    }
-    let ids = rawTweets.map{"\($0.id)"}
-    airport.enqueue(ids)
-    
-    /// Update boundaries.
-    let newSinceID = max(rawTweets.map(\.id).max(), Int64?(limitID))
-    UserDefaults.groupSuite.sinceID = newSinceID.string
-    Swift.debugPrint("newSinceID \(newSinceID ?? 0)")
-}
