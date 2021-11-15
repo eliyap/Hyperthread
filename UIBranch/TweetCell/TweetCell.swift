1deea0d8e41b10760503befaea8b5b8a4a952425
Ipsky <yap@usc.edu>
2021-11-14 21:09:58 -0800

    add color bar

--- UIBranch/TweetCell/TweetCell.swift
+++ UIBranch/TweetCell/TweetCell.swift
@@ -15,6 +15,7 @@ final class TweetCell: UITableViewCell {
     override var reuseIdentifier: String? { Self.reuseID }
     
     let depthStack = UIStackView()
+    let colorBar = UIButton()
     let depthSpacer = UIView()
     
     /// Component Views
@@ -26,8 +27,23 @@ final class TweetCell: UITableViewCell {
     // TODO: add profile image
     // TODO: add retweet marker
     
+    private let colorBarWidth: CGFloat = 1.5
+    private let inset: CGFloat = 8
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        /// Add color bar.
+        depthSpacer.addSubview(colorBar)
+        colorBar.translatesAutoresizingMaskIntoConstraints = false
+        colorBar.backgroundColor = .systemRed
+        colorBar.layer.cornerRadius = colorBarWidth / 2
+        NSLayoutConstraint.activate([
+            colorBar.widthAnchor.constraint(equalToConstant: colorBarWidth),
+            colorBar.trailingAnchor.constraint(equalTo: depthSpacer.trailingAnchor, constant: -inset),
+            colorBar.topAnchor.constraint(equalTo: depthSpacer.topAnchor),
+            colorBar.bottomAnchor.constraint(equalTo: depthSpacer.bottomAnchor),
+        ])
         
         /// Configure Depth Stack View.
         contentView.addSubview(depthStack)
@@ -36,10 +52,10 @@ final class TweetCell: UITableViewCell {
         depthStack.addArrangedSubview(depthSpacer)
         depthStack.addArrangedSubview(stackView)
         NSLayoutConstraint.activate([
-            depthStack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            depthStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
-            depthStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
-            depthStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            depthStack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            depthStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            depthStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            depthStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
         ])
 
         /// Configure Main Stack View.
