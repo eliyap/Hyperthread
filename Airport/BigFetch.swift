a64416696d9b4ed526efcd9841c2a7328ebf4d1d
Ipsky <yap@usc.edu>
2021-11-14 19:07:00 -0800

    split out orphan linking as its own function

--- Airport/BigFetch.swift
+++ Airport/BigFetch.swift
@@ -121,3 +121,63 @@ func furtherFetch(rawTweets: [RawHydratedTweet], rawUsers: [RawIncludeUser]) thr
     
     return idsToFetch
 }
+
+/// Tries to link Tweets to Conversations, and Conversations to Discussions.
+fileprivate func link(orphan: Conversation, idsToFetch: inout Set<Tweet.ID>, realm: Realm) -> Void {
+    /// Link to upstream's discussion, if possible.
+    if
+        let upstreamID = orphan.upstream,
+        let upstreamConvo = realm.conversation(id: upstreamID),
+        let upstream: Discussion = upstreamConvo.discussion.first
+    {
+        upstream.conversations.append(orphan)
+        upstream.update(with: orphan.tweets.map(\.createdAt).max())
+        return
+    }
+    /** Conclusion: upstream is either missing, or itself has no `Discussion`. **/
+    
+    /// Link the conversation to a local tweet, if needed and possible.
+    if
+        orphan.root == nil,
+        let local = realm.tweet(id: orphan.id)
+    {
+        orphan.root = local
+    }
+    
+    /// Check if root tweet was still not found.
+    guard let root: Tweet = orphan.root else {
+        idsToFetch.insert(orphan.id)
+        return
+    }
+    
+    /// Remove conversations that are standalone discussions.
+    guard let primaryReference: Tweet.ID = root.primaryReference else {
+        /// Recognize conversation as its own discussion.
+        orphan.upstream = root.id
+        realm.add(Discussion(root: orphan))
+        return
+    }
+    
+    /// Remove conversations with un-fetched upstream tweets.
+    guard let orphanRootReferenced: Tweet = realm.tweet(id: primaryReference) else {
+        /// Go fetch the upstream reference.
+        idsToFetch.insert(primaryReference)
+        return
+    }
+    
+    /// Set upstream conversation.
+    let upstreamID: Conversation.ID = orphanRootReferenced.conversation_id
+    orphan.upstream = upstreamID
+        
+    /// Inherit discussion, if possible.
+    if
+        let upstreamConvo = realm.conversation(id: upstreamID),
+        let upstreamDiscussion = upstreamConvo.discussion.first
+    {
+        upstreamDiscussion.conversations.append(orphan)
+        upstreamDiscussion.update(with: orphan.tweets.map(\.createdAt).max())
+    } else {
+        /// Go fetch the upstream conversation root.
+        idsToFetch.insert(upstreamID)
+    }
+}
