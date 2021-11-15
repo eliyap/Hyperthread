750e35a194bb3a9ca5cbff315b9ce4f3c791a3b7
Ipsky <yap@usc.edu>
2021-11-14 21:18:43 -0800

    add colors

--- UIBranch/AssetColors.swift
+++ UIBranch/AssetColors.swift
@@ -13,3 +13,14 @@ extension UIColor {
     static let cardSelected = UIColor(named: "cardSelected")!
     static let flat = UIColor(named: "flat")!
 }
+
+extension UIColor {
+    /// SC could be https://sixcolors.com/
+    /// or Southern California. Either one.
+    static let SCGreen = UIColor(named: "SC_Green")!
+    static let SCYellow = UIColor(named: "SC_Yellow")!
+    static let SCOrange = UIColor(named: "SC_Orange")!
+    static let SCRed = UIColor(named: "SC_Red")!
+    static let SCPurple = UIColor(named: "SC_Purple")!
+    static let SCBlue = UIColor(named: "SC_Blue")!
+}
