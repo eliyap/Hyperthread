dbad35c1cb5f804e27c7fdb3741c3c207a52f3e0
Ipsky <yap@usc.edu>
2021-11-21 08:51:15 -0800

    pass layout to subview

--- UIBranch/CardCell/CardBackground.swift
+++ UIBranch/CardCell/CardBackground.swift
@@ -11,9 +11,12 @@ final class CardBackground: UIButton {
     
     /// How far the view will be inset from its superview.
     private let inset: CGFloat
+    private let triangleView: TriangleView
     
     init(inset: CGFloat) {
         self.inset = inset
+        let size = inset * 4
+        self.triangleView = TriangleView(size: size)
         super.init(frame: .zero)
         
         /// Round corners.
@@ -23,18 +26,10 @@ final class CardBackground: UIButton {
         /// Clip triangle to rounded corner.
         layer.masksToBounds = true
         
-        let size = inset * 4
-        let triangleView = TriangleView(size: size)
         addSubview(triangleView)
         
         /// Align view to top right, with fixed size.
-        triangleView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            triangleView.topAnchor.constraint(equalTo: topAnchor),
-            triangleView.rightAnchor.constraint(equalTo: rightAnchor),
-            triangleView.heightAnchor.constraint(equalToConstant: size),
-            triangleView.widthAnchor.constraint(equalToConstant: size),
-        ])
+        triangleView.constrain(to: safeAreaLayoutGuide)
     }
     
     public func constrain(to guide: UILayoutGuide) -> Void {
@@ -56,6 +51,34 @@ final class CardBackground: UIButton {
     }
 }
 
+final class TriangleView: UIView {
+    
+    private let triangleLayer: TriangleLayer
+    private let size: CGFloat
+    
+    init(size: CGFloat) {
+        self.size = size
+        triangleLayer = TriangleLayer(size: size)
+        super.init(frame: CGRect(origin: .zero, size: CGSize(width: size, height: size)))
+        
+        layer.addSublayer(triangleLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func constrain(to guide: UILayoutGuide) -> Void {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: guide.topAnchor),
+            rightAnchor.constraint(equalTo: guide.rightAnchor),
+            heightAnchor.constraint(equalToConstant: size),
+            widthAnchor.constraint(equalToConstant: size),
+        ])
+    }
+}
+
 final class TriangleLayer: CAShapeLayer {
     
     let size: CGFloat
@@ -74,22 +97,6 @@ final class TriangleLayer: CAShapeLayer {
         self.path = path.cgPath
         
         fillColor = UIColor.systemRed.cgColor
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-final class TriangleView: UIView {
-    
-    private let triangleLayer: TriangleLayer
-    
-    init(size: CGFloat) {
-        triangleLayer = TriangleLayer(size: size)
-        super.init(frame: CGRect(origin: .zero, size: CGSize(width: size, height: size)))
-        
-        layer.addSublayer(triangleLayer)
     }
     
     required init?(coder: NSCoder) {
