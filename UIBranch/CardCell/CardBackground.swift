566c645140146e61b7d7c1456b623a5a05aa203e
Ipsky <yap@usc.edu>
2021-11-21 08:10:40 -0800

    split file

---
+++ UIBranch/CardCell/CardBackground.swift
@@ -0,0 +1,37 @@
+//
+//  CardBackground.swift
+//  UIBranch
+//
+//  Created by Secret Asian Man Dev on 21/11/21.
+//
+
+import UIKit
+
+final class CardBackground: UIButton {
+    
+    /// How far the view will be inset from its superview.
+    private let inset: CGFloat
+    
+    init(inset: CGFloat) {
+        self.inset = inset
+        super.init(frame: .zero)
+        
+        /// Round corners.
+        layer.cornerRadius = inset * 2
+        layer.cornerCurve = .continuous
+    }
+    
+    public func constrain(to guide: UILayoutGuide) -> Void {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: guide.topAnchor, constant: inset),
+            bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -inset),
+            leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: inset),
+            trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -inset),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- UIBranch/CardCell/CardTeaserCell.swift
+++ UIBranch/CardCell/CardTeaserCell.swift
@@ -159,32 +159,3 @@ final class TriangleLayer: CAShapeLayer {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
-final class CardBackground: UIButton {
-    
-    /// How far the view will be inset from its superview.
-    private let inset: CGFloat
-    
-    init(inset: CGFloat) {
-        self.inset = inset
-        super.init(frame: .zero)
-        
-        /// Round corners.
-        layer.cornerRadius = inset * 2
-        layer.cornerCurve = .continuous
-    }
-    
-    public func constrain(to guide: UILayoutGuide) -> Void {
-        translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            topAnchor.constraint(equalTo: guide.topAnchor, constant: inset),
-            bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -inset),
-            leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: inset),
-            trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -inset),
-        ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
