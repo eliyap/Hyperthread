8d2b3327b7ada6679bf81b26ae23cee5d48e0ef6
Ipsky <yap@usc.edu>
2021-11-21 13:45:52 -0800

    add config option

--- UIBranch/TweetCell/IconView.swift
+++ UIBranch/TweetCell/IconView.swift
@@ -15,8 +15,10 @@ final class IconView: UIStackView {
     private let label = UILabel()
     
     private let textStyle = UIFont.TextStyle.footnote
+    private let symbolConfig: UIImage.SymbolConfiguration?
     
     init(sfSymbol: String, symbolConfig: UIImage.SymbolConfiguration? = nil) {
+        self.symbolConfig = symbolConfig
         super.init(frame: .zero)
         
         /// Configure Main Stack View.
@@ -30,13 +32,7 @@ final class IconView: UIStackView {
 
         label.font = UIFont.preferredFont(forTextStyle: self.textStyle)
         
-        imageView.image = UIImage(systemName: sfSymbol)
-        var config = UIImage.SymbolConfiguration.init(paletteColors: [.secondaryLabel])
-        config = config.applying(UIImage.SymbolConfiguration(textStyle: self.textStyle))
-        if let other = symbolConfig {
-            config = config.applying(other)
-        }
-        imageView.preferredSymbolConfiguration = config
+        setImage(to: sfSymbol)
 
         /// Mute Colors.
         imageView.tintColor = .secondaryLabel
@@ -45,6 +41,16 @@ final class IconView: UIStackView {
 
     public func setText(to text: String) {
         label.text = text
+    }
+    
+    public func setImage(to sfSymbol: String) {
+        imageView.image = UIImage(systemName: sfSymbol)
+        var config = UIImage.SymbolConfiguration.init(paletteColors: [.secondaryLabel])
+        config = config.applying(UIImage.SymbolConfiguration(textStyle: self.textStyle))
+        if let other = symbolConfig {
+            config = config.applying(other)
+        }
+        imageView.preferredSymbolConfiguration = config
     }
 
     required init(coder: NSCoder) {
