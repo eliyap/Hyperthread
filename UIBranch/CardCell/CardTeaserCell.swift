0879a34fec7cf8f72a65f3912a68127df423870a
Ipsky <yap@usc.edu>
2021-11-21 12:10:59 -0800

    split file

--- UIBranch/CardCell/CardTeaserCell.swift
+++ UIBranch/CardCell/CardTeaserCell.swift
@@ -10,43 +10,6 @@ import RealmSwift
 import Realm
 import Twig
 
-final class MarkReadDaemon {
-    
-    let token: NotificationToken
-    
-    public init(token: NotificationToken) {
-        self.token = token
-    }
-    
-    private let realm = try! Realm()
-    
-    /// `seen` indicates whether the discussion was fully visible for the user to read.
-    var indices: [IndexPath: Discussion] = [:]
-    
-    func associate(_ path: IndexPath, with discussion: Discussion) {
-        indices[path] = discussion
-    }
-    
-    /// Marks the index path as having been seen.
-    func mark(_ path: IndexPath) {
-        guard let discussion: Discussion = indices[path] else {
-            Swift.debugPrint("Missing key \(path)")
-            return
-        }
-        if discussion.tweets.count == 1 {
-            do {
-                try realm.write(withoutNotifying: [token]) {
-                    discussion.read = .read
-                }
-            } catch {
-                // TODO: log non-critical failure.
-                assert(false, "\(error)")
-                return
-            }
-        }
-    }
-}
-
 final class CardTeaserCell: UITableViewCell {
     
     public static let reuseID = "CardTeaserCell"
