74dc7acf730a86b6a18f28c6a8f54eea7c828ea2
Ipsky <yap@usc.edu>
2021-11-23 13:33:34 -0800

    move symbol config

--- UIBranch/TweetCell/IconView.swift
+++ UIBranch/TweetCell/IconView.swift
@@ -11,14 +11,13 @@ import Twig
 
 final class IconView: UIStackView {
     
-    private let imageView = IconImageView()
+    private let imageView: IconImageView
     private let label = UILabel()
     
-    private let textStyle = UIFont.TextStyle.footnote
-    private let symbolConfig: UIImage.SymbolConfiguration?
+    public static let textStyle = UIFont.TextStyle.footnote
     
     init(sfSymbol: String, symbolConfig: UIImage.SymbolConfiguration? = nil) {
-        self.symbolConfig = symbolConfig
+        imageView = IconImageView(symbolConfig: symbolConfig)
         super.init(frame: .zero)
         
         /// Configure Main Stack View.
@@ -29,7 +28,7 @@ final class IconView: UIStackView {
         addArrangedSubview(imageView)
         addArrangedSubview(label)
 
-        label.font = UIFont.preferredFont(forTextStyle: self.textStyle)
+        label.font = UIFont.preferredFont(forTextStyle: Self.textStyle)
         
         setImage(to: sfSymbol)
 
@@ -43,12 +42,6 @@ final class IconView: UIStackView {
     
     public func setImage(to sfSymbol: String) {
         imageView.image = UIImage(systemName: sfSymbol)
-        var config = UIImage.SymbolConfiguration.init(paletteColors: [.secondaryLabel])
-        config = config.applying(UIImage.SymbolConfiguration(textStyle: self.textStyle))
-        if let other = symbolConfig {
-            config = config.applying(other)
-        }
-        imageView.preferredSymbolConfiguration = config
     }
 
     required init(coder: NSCoder) {
@@ -57,11 +50,25 @@ final class IconView: UIStackView {
 }
 
 final class IconImageView: UIImageView {
-    init() {
+    
+    public init(symbolConfig: UIImage.SymbolConfiguration? = nil) {
         super.init(frame: .zero)
         tintColor = .secondaryLabel
         contentMode = .scaleAspectFit
+        
+        /// Applying a text style causes image to scale with Dynamic Type.
+        var config = UIImage.SymbolConfiguration.init(paletteColors: [.secondaryLabel])
+        config = config.applying(UIImage.SymbolConfiguration(textStyle: IconView.textStyle))
+        
+        /// Combine with other configuration.
+        if let other = symbolConfig {
+            config = config.applying(other)
+        }
+        
+        preferredSymbolConfiguration = config
     }
+    
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
