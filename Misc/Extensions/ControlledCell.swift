e22d215d2bd06858ecb8566591530e95b0c4b66e
Ipsky <yap@usc.edu>
2021-12-05 12:12:44 -0800

    define possible superclass

---
+++ Misc/Extensions/ControlledCell.swift
@@ -0,0 +1,36 @@
+//
+//  ControlledCell.swift
+//  UIBranch
+//
+//  Created by Secret Asian Man Dev on 5/12/21.
+//
+
+import UIKit
+
+/**
+ A `UITableViewCell` which owns a `UIViewController`.
+ Though this runs against the spirit of Cocoa, it is our method to include child View Controllers.
+ */
+final class ControlledCell: UITableViewCell {
+
+    /// Internal view controller.
+    public let controller = UIViewController()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        /// Add to hierarchy and pin edges.
+        contentView.addSubview(controller.view)
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            controller.view.topAnchor.constraint(equalTo: contentView.topAnchor),
+            controller.view.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            controller.view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            controller.view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
