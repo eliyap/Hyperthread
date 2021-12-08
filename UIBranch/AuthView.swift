83eb8126da3e70b65a52430e791aa1d95269f299
Ipsky <yap@usc.edu>
2021-12-07 18:46:16 -0800

    fix warning

--- UIBranch/AuthView.swift
+++ UIBranch/AuthView.swift
@@ -59,7 +59,19 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: ASWebAuthenticationPresentationContextProviding {
     func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
-        return UIApplication.shared.windows.filter { $0.isKeyWindow }.first!
+        /// Source: https://stackoverflow.com/a/68989580/12395667
+        let window = UIApplication.shared.connectedScenes
+            /// Keep only active scenes, onscreen and visible to the user
+            .filter { $0.activationState == .foregroundActive }
+            /// Keep only the first `UIWindowScene`
+            .first(where: { $0 is UIWindowScene })
+            /// Get its associated windows
+            .flatMap({ $0 as? UIWindowScene })?.windows
+            /// Finally, keep only the key window
+            .first(where: \.isKeyWindow)
+        
+        /// Blindly hope that this works.
+        return window!
     }
 }
 
