5180744ab1269786d3b86884e47a2f664a117404
Ipsky <yap@usc.edu>
2021-12-07 18:55:34 -0800

    refactor to fix warnings

--- UIBranch/AuthView.swift
+++ UIBranch/AuthView.swift
@@ -59,19 +59,20 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: ASWebAuthenticationPresentationContextProviding {
     func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
-        /// Source: https://stackoverflow.com/a/68989580/12395667
-        let window = UIApplication.shared.connectedScenes
-            /// Keep only active scenes, onscreen and visible to the user
-            .filter { $0.activationState == .foregroundActive }
-            /// Keep only the first `UIWindowScene`
-            .first(where: { $0 is UIWindowScene })
-            /// Get its associated windows
-            .flatMap({ $0 as? UIWindowScene })?.windows
-            /// Finally, keep only the key window
-            .first(where: \.isKeyWindow)
-        
-        /// Blindly hope that this works.
-        return window!
+        getKeyWindow()!
     }
 }
 
+/// Source: https://stackoverflow.com/a/68989580/12395667
+func getKeyWindow() -> UIWindow? {
+    getWindowScene()?.keyWindow
+}
+
+func getWindowScene() -> UIWindowScene? {
+    UIApplication.shared.connectedScenes
+        /// Keep only active scenes, onscreen and visible to the user
+        .filter { $0.activationState == .foregroundActive }
+        /// Keep only the first `UIWindowScene`
+        .compactMap{ $0 as? UIWindowScene }
+        .first
+}
