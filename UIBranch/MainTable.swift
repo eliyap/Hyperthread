d1fd6c4bafb6b6fac45069b76c4bc54dc209be15
Ipsky <yap@usc.edu>
2021-12-07 16:41:14 -0800

    handle edge case generated by my fake tweet

--- UIBranch/MainTable.swift
+++ UIBranch/MainTable.swift
@@ -116,7 +116,7 @@ final class MainTable: UITableViewController {
             fatalError("Failed to create or cast new cell!")
         }
         let tweet = realm.tweet(id: discussion.id)!
-        let author = realm.user(id: tweet.authorID)!
+        let author = realm.user(id: tweet.authorID)
         cell.configure(discussion: discussion, tweet: tweet, author: author, realm: realm)
         cell.resetStyle()
         mrd.associate(indexPath, with: discussion)
