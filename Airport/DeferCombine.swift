cc15e1d444dbb93126571846533aa79041925195
Ipsky <yap@usc.edu>
2021-12-15 14:48:39 -0800

    add operator

--- Airport/DeferCombine.swift
+++ Airport/DeferCombine.swift
@@ -8,6 +8,39 @@
 import Foundation
 import Combine
 
+extension Publisher {
+    /// Notes: `Output` is the output associated-type of `Publisher`, similarly `Failure` is the `Publisher`'s error type.
+
+    /// Collects elements from the source sequence until `boundary` fires, or buffer reachers`size`.
+    /// Then it emits the elements as an array and begins collecting again.
+    func deferredBuffer<BufferType, BufferItem>(
+        _ zipperType: BufferType.Type,
+        timer: TimeInterval
+    ) -> AnyPublisher<(Output, BufferItem), Failure> where
+        BufferType: DeferredBuffer<Output, BufferItem, Failure>
+    {
+        let subject = PassthroughSubject<(Output, BufferItem), Failure>()
+        let zipper = BufferType(emitter: subject, timer: timer)
+        
+        let disposable: AnyCancellable = self.sink(
+            receiveCompletion: { _ in
+                fatalError("Should never be completed!")
+            },
+            receiveValue: { element in
+                zipper.store(element)
+            }
+        )
+
+        return subject
+            .handleEvents(receiveCancel: disposable.cancel)
+            .eraseToAnyPublisher()
+    }
+}
+
+
+/**
+ A custom `Combine` operator which
+ */
 class DeferredBuffer<Input, Output, Failure: Error> {
     
     /// Stores input values waiting for an output to be paired with.
