add72dcd0c0a2811af09c13c2adf2114acfdce99
eliyap <yap@usc.edu>
2021-12-15 20:50:18 -0800

    new files

---
+++ Airport/DateWindow.swift
@@ -0,0 +1,8 @@
+//
+//  DateWindow.swift
+//  Hyperthread
+//
+//  Created by Secret Asian Man Dev on 15/12/21.
+//
+
+import Foundation
---
+++ Airport/TimelineConduit.swift
@@ -0,0 +1,8 @@
+//
+//  TimelineConduit.swift
+//  Hyperthread
+//
+//  Created by Secret Asian Man Dev on 15/12/21.
+//
+
+import Foundation
