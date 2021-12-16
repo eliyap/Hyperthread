33eae06e2899123c49cda5c007873344c94e8571
eliyap <yap@usc.edu>
2021-12-16 00:10:12 -0800

    build recursive fetcher

--- Airport/TimelineConduit.swift
+++ Airport/TimelineConduit.swift
@@ -18,12 +18,29 @@ final class TimelineConduit {
     
     public init(credentials: OAuthCredentials) {
         pipeline = intake
-            .flatMap {
-                userTimelinePublisher(userID: $0.id, credentials: credentials, startTime: $0.startTime, endTime: $0.endTime)
+            .flatMap { (request: Request) -> AnyPublisher in
+                let publisher = PassthroughSubject<([RawHydratedTweet], [RawIncludeUser], [RawIncludeMedia]), Error>()
+                Task {
+                    var nextToken: String? = nil
+                    repeat {
+                        let (tweets, users, media, token) = try await userTimeline(
+                            userID: request.id,
+                            credentials: credentials,
+                            startTime: request.startTime,
+                            endTime: request.endTime,
+                            nextToken: nextToken
+                        )
+                        publisher.send((tweets, users, media))
+                        Swift.debugPrint("\(tweets.count) Tweets", tweets.map(\.text))
+                        nextToken = token
+                    } while (nextToken != nil)
+                    
+                }
+                return publisher.eraseToAnyPublisher()
             }
-            .sink(receiveCompletion: { (completion: Subscribers.Completion<URLError>) in
+            .sink(receiveCompletion: { (completion: Subscribers.Completion<Error>) in
                 ///
-            }, receiveValue: { (data: Data) in
+            }, receiveValue: { ([RawHydratedTweet], [RawIncludeUser], [RawIncludeMedia]) in
                 ///
             })
     }
