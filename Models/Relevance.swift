070945ae69d6c36ed1153c987ca2f1f2a9211110
eliyap <yap@usc.edu>
2021-12-21 07:01:18 +0800

    extend relevance to allow finding max

--- Models/Relevance.swift
+++ Models/Relevance.swift
@@ -25,3 +25,13 @@ internal enum Relevance: Int {
     /// i.e. this Tweet is why you see this `Discussion`.
     case discussion = 999
 }
+
+/** Permit us to find the "maximum" enum.
+    Allows us to easily determine the highest `Relevance`.
+ */
+extension Collection where Element: RawRepresentable, Element.RawValue: Comparable {
+    func max() -> Element? {
+        guard let maxRaw = map(\.rawValue).max() else { return nil }
+        return .init(rawValue: maxRaw)
+    }
+}
