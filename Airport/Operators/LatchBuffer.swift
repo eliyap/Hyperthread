7232b2489a34a0e182330803ca88b3baf9a2f5a9
eliyap <yap@usc.edu>
2021-12-21 17:40:09 +0800

    move files & comment

--- Airport/Operators/LatchBuffer.swift
+++ Airport/Operators/LatchBuffer.swift
@@ -4,13 +4,18 @@ import Foundation
 import Combine
 
 extension Publisher {
-    /// Notes: `Output` is the output associated-type of `Publisher`, similarly `Failure` is the `Publisher`'s error type.
-
-    /// Collects elements from the source sequence until `boundary` fires, or buffer reachers`size`.
+    
+    /// Collects elements from the upstream sequence until `latch` "opens" (i.e. receives a value),
+    /// or buffer reachers`size`.
     /// Then it emits the elements as an array and begins collecting again.
+    ///
+    /// Analagous to a spring loaded trapdoor, which releases stuff accumulated above it when the weight
+    /// forces the trapdoor open, or the trapdoor is opened manually.
+    ///
+    /// Notes: `Output` is the output associated-type of `Publisher`, similarly `Failure` is the `Publisher`'s error type.
     func buffer<BoundaryPublisher: Publisher, BoundaryItem>(
         size: UInt,
-        _ boundary: BoundaryPublisher
+        _ latch: BoundaryPublisher
     ) -> AnyPublisher<[Output], Failure> where
         BoundaryPublisher.Output == BoundaryItem
     {
@@ -19,7 +24,7 @@ extension Publisher {
         var buffer: [Output] = []
         let lock = NSRecursiveLock()
 
-        let boundaryDisposable: AnyCancellable = boundary.sink(
+        let boundaryDisposable: AnyCancellable = latch.sink(
             receiveCompletion: { _ in },
             receiveValue: { _ in
                 lock.lock();
