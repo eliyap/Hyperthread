23f10770a66a3d62df0c04fdb75739d9d9d52414
eliyap <yap@usc.edu>
2021-12-22 08:43:00 +0800

    refactor to use enum method

--- UIBranch/TweetCell/TimeDifference.swift
+++ UIBranch/TweetCell/TimeDifference.swift
@@ -68,32 +68,6 @@ func approximateTimeSince(_ date: Date) -> Elapsed {
 
 /// A very rough method of creating a teeny timestamp.
 func approximateTimeSince(_ date: Date) -> String {
-    let seconds = date.distance(to: Date())
-    if seconds < 60 {
-        return "\(Int(seconds))s"
-    }
-    
-    let minutes = seconds / 60
-    if minutes < 60 {
-        return "\(Int(minutes))m"
-    }
-
-    let hours = minutes / 60
-    if hours < 24 {
-        return "\(Int(hours))h"
-    }
-
-    let days = hours / 24
-    if days < 7 {
-        return "\(Int(days))d"
-    }
-
-    let weeks = days / 7
-    if days < 365 {
-        return "\(Int(weeks))w"
-    }
-
-    /// - Note: Wrong! But probably close enough.
-    let years = days / 365
-    return "\(Int(years))y"
+    let interval: Elapsed = approximateTimeSince(date)
+    return interval.description
 }
