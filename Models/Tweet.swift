216b40b218e6df940b4a22e7b2103d3d48b1a45f
eliyap <yap@usc.edu>
2021-12-24 17:49:23 +0800

    define reference set

--- Models/Tweet.swift
+++ Models/Tweet.swift
@@ -10,6 +10,22 @@ import RealmSwift
 import Realm
 import Twig
 
+/**
+ Represents the set of references held by a Tweet.
+ */
+internal struct ReferenceSet: OptionSet {
+    var rawValue: Int
+    
+    typealias RawValue = Int
+    
+    static let reply = Self(rawValue: 1 << 0)
+    static let quote = Self(rawValue: 1 << 1)
+    static let retweet = Self(rawValue: 1 << 2)
+    
+    static let empty: Self = []
+    static let all: Self = [.reply, .quote, .retweet]
+}
+
 final class Tweet: Object, Identifiable, AuthorIdentifiable, TweetIdentifiable {
     
     /// Twitter API `id`.
