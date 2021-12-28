55c580e536463e775b65212bd7dd1fe460c56cf2
eliyap <yap@usc.edu>
2021-12-28 08:53:42 +0800

    flesh out userdefaults

--- Airport/DateWindow.swift
+++ Airport/DateWindow.swift
@@ -11,6 +11,21 @@ public struct DateWindow {
     let start: Date
     let duration: TimeInterval
     var end: Date { start.addingTimeInterval(duration) }
+    
+    init(start: Date, duration: TimeInterval) {
+        precondition(duration >= 0, "Duration must be non-negative!")
+        self.start = start
+        self.duration = duration
+    }
+    
+    init(start: Date, end: Date) {
+        precondition(end >= start, "End cannot be after start!")
+        self.init(start: start, duration: end.timeIntervalSince(start))
+    }
+    
+    public static func new() -> DateWindow {
+        .init(start: Date(), duration: .zero)
+    }
 }
 
 extension DateWindow: Codable { }
@@ -19,8 +34,21 @@ internal extension UserDefaults {
     var userTimelineWindow: DateWindow {
         get {
             guard let data = object(forKey: #function) as? Data else {
-                
+                DefaultsLog.debug("No Date Window found.", print: true, true)
+                return .new()
             }
+            guard let loaded = try? JSONDecoder().decode(DateWindow.self, from: data) else {
+                assert(false, "Could not decode \(DateWindow.self)")
+                return .new()
+            }
+            return loaded
+        }
+        set {
+            guard let encoded = try? JSONEncoder().encode(newValue) else {
+                assert(false, "Could not encode!")
+                return
+            }
+            set(encoded, forKey: #function)
         }
     }
 }
