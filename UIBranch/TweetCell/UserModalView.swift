d8ee318d45adc103f6339a00e38f50182f11617c
eliyap <yap@usc.edu>
2021-12-29 14:41:16 +0800

    pin stack view

--- UIBranch/TweetCell/UserModalView.swift
+++ UIBranch/TweetCell/UserModalView.swift
@@ -10,11 +10,30 @@ import UIKit
 
 #warning("View Incomplete")
 final class UserModalViewController: UIViewController {
-    
+    /**
+    set up stack view, pin constraints,
+    add username and handle label view, check for text scaling
+    add following / not following button,
+    hook up to realm stuff.
+    */
+
+    private let stackView: UIStackView
+
     init() {
         super.init(nibName: nil, bundle: nil)
         modalPresentationStyle = .automatic
         view.backgroundColor = .systemRed
+
+        view.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
     
     required init?(coder: NSCoder) {
