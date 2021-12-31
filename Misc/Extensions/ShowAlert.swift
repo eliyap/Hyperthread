efeb5b76a2a6243b0570bc5f7022bd4ab4d12746
eliyap <yap@usc.edu>
2021-12-31 14:17:26 +0800

    swap in convenience method

--- Misc/Extensions/ShowAlert.swift
+++ Misc/Extensions/ShowAlert.swift
@@ -8,12 +8,7 @@
 import UIKit
 
 func NOT_IMPLEMENTED() -> Void {
-    guard let scene = getWindowScene() else { return }
-    guard let vc = scene.keyWindow?.rootViewController else { return }
-    let ac = UIAlertController(title: "Right now, this doesn't do anything", message: nil, preferredStyle: .alert)
-    let ok = UIAlertAction(title: "😞", style: .default) { _ in }
-    ac.addAction(ok)
-    vc.present(ac, animated: true)
+    showAlert(title: "Right now, this doesn't do anything", message: nil, action: "😞")
 }
 
 func showAlert(title: String? = "⚠️ Error! ⚠️", message: String?, action: String = "OK") -> Void {
