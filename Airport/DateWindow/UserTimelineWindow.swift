716088af05e3ffe6eb98e76ecd95234b6d3b039e
eliyap <yap@usc.edu>
2022-01-01 12:57:19 +0800

    define convenience methods

--- Airport/DateWindow/UserTimelineWindow.swift
+++ Airport/DateWindow/UserTimelineWindow.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import Twig
+import BlackBox
 
 internal extension UserDefaults {
     /**
@@ -31,5 +33,36 @@ internal extension UserDefaults {
             }
             set(encoded, forKey: #function)
         }
+    }
+}
+
+internal extension DateWindow {
+    /// Intended for use with the Home Timeline return value.
+    init?(rawData: RawData) {
+        let tweets: [RawHydratedTweet] = rawData.0
+        guard tweets.isNotEmpty else {
+            return nil
+        }
+        let minDate = tweets.map(\.created_at).min()!
+        let maxDate = tweets.map(\.created_at).max()!
+        self.init(start: minDate, end: maxDate)
+    }
+}
+
+internal extension DateWindow {
+    
+    func overlaps(with other: DateWindow) -> Bool {
+        other.end > self.start && self.end > other.start
+    }
+    
+    func union(_ other: DateWindow) -> DateWindow {
+        if overlaps(with: other) == false {
+            Logger.general.warning("No overlap between DateWindows \(self) and \(other)")
+        }
+        
+        return .init(
+            start: min(self.start, other.start),
+            end: max(self.end, other.end)
+        )
     }
 }
