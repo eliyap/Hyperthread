411c3405108045aed14a53bd7e2b731b0d1c4b3e
eliyap <yap@usc.edu>
2022-01-04 12:28:59 +0800

    split file

--- UIBranch/CardCell/CardHeaderCell.swift
+++ UIBranch/CardCell/CardHeaderCell.swift
@@ -122,23 +122,3 @@ extension TweetViewDelegate {
         }
     }
 }
-
-struct HashtagURL {
-    public static let scheme = "hashtag"
-    static func urlString(tag: Tag) -> String {
-        "\(scheme)://\(tag.tag)"
-    }
-    static func tag(from url: URL) -> User.ID {
-        return url.host ?? ""
-    }
-}
-
-struct UserURL {
-    public static let scheme = "handle"
-    static func urlString(mention: Mention) -> String {
-        "\(scheme)://\(mention.id)"
-    }
-    static func id(from url: URL) -> User.ID {
-        url.host ?? ""
-    }
-}
