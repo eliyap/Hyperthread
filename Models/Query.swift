95f77797fd6537977a85119a5a57c87303fb758c
eliyap <yap@usc.edu>
2022-01-17 07:48:08 -0800

    split file

--- Models/Query.swift
+++ Models/Query.swift
@@ -65,28 +65,3 @@ func findMissingMentions(
     return Set(mentionedIDs)
         .filter { fetchedIDs.contains($0) == false }
 }
-
-extension Realm {
-    func updateRelevanceOnFollow(_ token: TransactionToken, userID: User.ID) -> Void {
-        let usersTweets = objects(Tweet.self)
-            .filter(NSPredicate(format: "\(Tweet.authorIDPropertyName) == %@", userID))
-        
-        /// Check that returned set is non-empty.
-        ModelLog.debug("Setting relevance for \(usersTweets.count) tweets.", print: true, true)
-        
-        /// Update all relevance metrics.
-        for tweet in usersTweets {
-            tweet.relevance = .init(tweet: tweet, following: [userID])
-        }
-    }
-    
-    func updateRelevanceOnUnfollow(_ token: TransactionToken, userID: User.ID) -> Void {
-        let usersTweets = objects(Tweet.self)
-            .filter(NSPredicate(format: "\(Tweet.authorIDPropertyName) == %@", userID))
-        
-        /// Update all relevance metrics.
-        for tweet in usersTweets {
-            tweet.relevance = .irrelevant
-        }
-    }
-}
