d05ada20663907687a6903d487a79f4bb0784bec
eliyap <yap@usc.edu>
2022-01-17 07:43:52 -0800

    split file

---
+++ Engine/StoreFollowing.swift
@@ -0,0 +1,38 @@
+//
+//  StoreFollowing.swift
+//  Hyperthread
+//
+//  Created by Secret Asian Man Dev on 17/1/22.
+//
+
+import Foundation
+import RealmSwift
+
+internal extension Realm {
+    /// Write following users to disk.
+    func storeFollowing<RawUserCollection>(raw: RawUserCollection) throws -> Void
+    where
+        RawUserCollection: Collection,
+        RawUserCollection.Element == RawUser
+    {
+        try write {
+            /// Remove users who are no longer being followed.
+            followingUsers()
+                .filter { user in
+                    /// Find users who were marked as followed but are now missing.
+                    raw.contains(where: {user.id == $0.id}) == false
+                }
+                .forEach { user in
+                    #warning("TODO: perform unfollow actions!")
+                    user.following = false
+                }
+            
+            /// Write out users to account for possible new users.
+            raw.forEach {
+                #warning("TODO: perform follow actions!")
+                let user = User(raw: $0, following: true)
+                add(user, update: .all)
+            }
+        }
+    }
+}
