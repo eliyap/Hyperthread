ebd96d8c04d54a57c57de2d287c456e853c89b10
eliyap <yap@usc.edu>
2022-01-18 15:41:59 -0800

    design a simple POC

--- UIBranch/CardCell/MainHeaderCell.swift
+++ UIBranch/CardCell/MainHeaderCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+/// Currently unused...
 final class MainHeaderCell: UITableViewCell {
     
     public static let reuseID = "MainHeaderCell"
@@ -14,17 +15,33 @@ final class MainHeaderCell: UITableViewCell {
     
     /// Component views.
     let cardBackground = CardBackground()
+    let stackView = UIStackView()
+    
+    private lazy var inset: CGFloat = CardTeaserCell.borderInset
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         /// Configure background.
         contentView.addSubview(cardBackground)
-        cardBackground.constrain(to: contentView.safeAreaLayoutGuide)
+        cardBackground.constrain(to: safeAreaLayoutGuide)
 
+        /// Configure Main Stack View.
+        contentView.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset * 2),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset * 2),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset * 2),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset * 2),
+        ])
+
+        
         let label = UILabel()
         label.text = "Hello"
-        contentView.addSubview(label)
+        stackView.addArrangedSubview(label)
     }
     
     required init?(coder: NSCoder) {
