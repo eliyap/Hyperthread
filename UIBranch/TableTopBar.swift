10f47006b4114b1a52b377570e26688b64bf9765
eliyap <yap@usc.edu>
2022-01-20 06:02:44 -0800

    ws

--- UIBranch/TableTopBar.swift
+++ UIBranch/TableTopBar.swift
@@ -26,23 +26,23 @@ internal struct UserMessage {
 }
 
 final class TableTopBar: UIVisualEffectView {
-    
+
     private let stackView: UIStackView = .init()
     private let label: UILabel = .init()
     private let loadingView: UIActivityIndicatorView = .init()
     private let iconView: UIImageView = .init()
-    
+
     private var observers: Set<AnyCancellable> = []
     private var heightConstraint: NSLayoutConstraint? = nil
-    
+
     init(loadingConduit: UserMessageConduit) {
         super.init(effect: UIBlurEffect(style: .systemMaterial))
-        
+
         contentView.addSubview(stackView)
         stackView.axis = .horizontal
         stackView.alignment = .center
         stackView.distribution = .equalSpacing
-        
+
         stackView.addArrangedSubview(iconView)
         stackView.addArrangedSubview(label)
         stackView.addArrangedSubview(loadingView)
@@ -50,19 +50,19 @@ final class TableTopBar: UIVisualEffectView {
         iconView.preferredSymbolConfiguration = .init(textStyle: .body)
             .applying(UIImage.SymbolConfiguration(paletteColors: [.label]))
         iconView.contentMode = .scaleAspectFit
-        
+
         loadingView.hidesWhenStopped = true
-        
+
         heightConstraint = contentView.heightAnchor.constraint(equalToConstant: .zero)
         heightConstraint?.isActive = true
-        
+
         label.setContentHuggingPriority(.required, for: .vertical)
-        
+
         loadingConduit
             .sink { [weak self] in self?.setState($0) }
             .store(in: &observers)
     }
-    
+
     public func constrain(to view: UIView) -> Void {
         self.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -72,16 +72,16 @@ final class TableTopBar: UIVisualEffectView {
             self.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             self.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
-        
+
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         let topMargin = stackView.topAnchor.constraint(lessThanOrEqualTo: topAnchor, constant: CardTeaserCell.borderInset)
         topMargin.priority = .defaultHigh
         topMargin.isActive = true
         let bottomMargin = stackView.bottomAnchor.constraint(greaterThanOrEqualTo: bottomAnchor, constant: -CardTeaserCell.borderInset)
         bottomMargin.priority = .defaultHigh
         bottomMargin.isActive = true
-        
+
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(lessThanOrEqualTo: topAnchor, constant: CardTeaserCell.borderInset),
             stackView.bottomAnchor.constraint(greaterThanOrEqualTo: bottomAnchor, constant: -CardTeaserCell.borderInset),
@@ -93,7 +93,7 @@ final class TableTopBar: UIVisualEffectView {
             label.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
         ])
     }
-    
+
     public func setState(_ message: UserMessage?) -> Void {
         /// UI code **must** run on the main thread!
         DispatchQueue.main.async { [weak self] in
@@ -102,37 +102,37 @@ final class TableTopBar: UIVisualEffectView {
                 assert(false)
                 return
             }
-            
+
             guard let message = message else {
                 replace(object: self, on: \TableTopBar.heightConstraint, with: self.contentView.heightAnchor.constraint(equalToConstant: .zero))
                 return
             }
-            
+
             replace(object: self, on: \TableTopBar.heightConstraint, with: nil)
-            
+
             switch message.category {
-            
+
             case .loading:
                 self.iconView.image = UIImage(systemName: "arrow.down.circle.fill")
                 self.label.text = "Loading..."
                 self.loadingView.startAnimating()
-            
+
             case .loaded:
                 self.iconView.image = UIImage(systemName: "checkmark.circle.fill")
                 self.label.text = "Done."
                 self.loadingView.stopAnimating()
-            
+
             default:
                 #warning("TODO")
                 break
             }
         }
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     deinit {
         observers.forEach { $0.cancel() }
     }
