fe5942a79ef3f8fd1ee663af6bd3f93b12cfbe88
eliyap <yap@usc.edu>
2022-01-20 06:25:09 -0800

    rework view to support a nice animation

--- UIBranch/TableTopBar.swift
+++ UIBranch/TableTopBar.swift
@@ -27,37 +27,17 @@ internal struct UserMessage {
 
 final class TableTopBar: UIVisualEffectView {
 
-    private let stackView: UIStackView = .init()
-    private let label: UILabel = .init()
-    private let loadingView: UIActivityIndicatorView = .init()
-    private let iconView: UIImageView = .init()
-
+    private let barContents: BarContents = .init()
+    
     private var observers: Set<AnyCancellable> = []
     private var heightConstraint: NSLayoutConstraint? = nil
 
     init(loadingConduit: UserMessageConduit) {
         super.init(effect: UIBlurEffect(style: .systemMaterial))
 
-        contentView.addSubview(stackView)
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.distribution = .equalSpacing
-
-        stackView.addArrangedSubview(iconView)
-        stackView.addArrangedSubview(label)
-        stackView.addArrangedSubview(loadingView)
-
-        iconView.preferredSymbolConfiguration = .init(textStyle: .body)
-            .applying(UIImage.SymbolConfiguration(paletteColors: [.label]))
-        iconView.contentMode = .scaleAspectFit
-
-        loadingView.hidesWhenStopped = true
-
-        heightConstraint = contentView.heightAnchor.constraint(equalToConstant: .zero)
-        heightConstraint?.isActive = true
-
-        label.setContentHuggingPriority(.required, for: .vertical)
-
+        contentView.addSubview(barContents)
+        barContents.isHidden = true
+        
         loadingConduit
             .sink { [weak self] in self?.setState($0) }
             .store(in: &observers)
@@ -73,24 +53,13 @@ final class TableTopBar: UIVisualEffectView {
             self.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
 
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-
-        let topMargin = stackView.topAnchor.constraint(lessThanOrEqualTo: topAnchor, constant: CardTeaserCell.borderInset)
-        topMargin.priority = .defaultHigh
-        topMargin.isActive = true
-        let bottomMargin = stackView.bottomAnchor.constraint(greaterThanOrEqualTo: bottomAnchor, constant: -CardTeaserCell.borderInset)
-        bottomMargin.priority = .defaultHigh
-        bottomMargin.isActive = true
-
+        barContents.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(lessThanOrEqualTo: topAnchor, constant: CardTeaserCell.borderInset),
-            stackView.bottomAnchor.constraint(greaterThanOrEqualTo: bottomAnchor, constant: -CardTeaserCell.borderInset),
+            barContents.topAnchor.constraint(equalTo: topAnchor),
+            barContents.bottomAnchor.constraint(equalTo: bottomAnchor),
             /// Keep stack contents away from notch / home indicator in iPhone X portrait mode.
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: CardTeaserCell.borderInset),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -CardTeaserCell.borderInset),
-            /// Pin to label.
-            label.topAnchor.constraint(equalTo: stackView.topAnchor),
-            label.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
+            barContents.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            barContents.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
         ])
     }
 
@@ -102,29 +71,16 @@ final class TableTopBar: UIVisualEffectView {
                 assert(false)
                 return
             }
-
-            guard let message = message else {
-                replace(object: self, on: \TableTopBar.heightConstraint, with: self.contentView.heightAnchor.constraint(equalToConstant: .zero))
-                return
-            }
-
-            replace(object: self, on: \TableTopBar.heightConstraint, with: nil)
-
-            switch message.category {
-
-            case .loading:
-                self.iconView.image = UIImage(systemName: "arrow.down.circle.fill")
-                self.label.text = "Loading..."
-                self.loadingView.startAnimating()
-
-            case .loaded:
-                self.iconView.image = UIImage(systemName: "checkmark.circle.fill")
-                self.label.text = "Done."
-                self.loadingView.stopAnimating()
-
-            default:
-                #warning("TODO")
-                break
+            UIView.transition(with: self.barContents, duration: 0.25, options: .transitionCrossDissolve) {
+                if let message = message {
+                    self.barContents.isHidden = false
+                    self.barContents.configure(category: message.category)
+                    let inset = CardTeaserCell.borderInset
+                    self.barContents.layoutMargins = .init(top: inset, left: inset, bottom: inset, right: inset)
+                } else {
+                    self.barContents.isHidden = true
+                    self.barContents.layoutMargins = .zero
+                }
             }
         }
     }
@@ -135,5 +91,56 @@ final class TableTopBar: UIVisualEffectView {
 
     deinit {
         observers.forEach { $0.cancel() }
+    }
+}
+
+fileprivate final class BarContents: UIStackView {
+    
+    private let label: UILabel = .init()
+    private let loadingView: UIActivityIndicatorView = .init()
+    private let iconView: UIImageView = .init()
+    
+    lazy var inset = CardTeaserCell.borderInset
+    init() {
+        super.init(frame: .zero)
+        axis = .horizontal
+        alignment = .center
+        distribution = .equalSpacing
+        
+        isLayoutMarginsRelativeArrangement = true
+        // layoutMargins = .init(top: inset, left: inset, bottom: inset, right: inset)
+
+        addArrangedSubview(iconView)
+        addArrangedSubview(label)
+        addArrangedSubview(loadingView)
+        
+        iconView.preferredSymbolConfiguration = .init(textStyle: .body)
+            .applying(UIImage.SymbolConfiguration(paletteColors: [.label]))
+        iconView.contentMode = .scaleAspectFit
+
+        loadingView.hidesWhenStopped = true
+    }
+    
+    public func configure(category: UserMessage.Category) -> Void {
+        switch category {
+
+        case .loading:
+            self.iconView.image = UIImage(systemName: "arrow.down.circle.fill")
+            self.label.text = "Loading..."
+            self.loadingView.startAnimating()
+
+        case .loaded:
+            self.iconView.image = UIImage(systemName: "checkmark.circle.fill")
+            self.label.text = "Done."
+            self.loadingView.stopAnimating()
+
+        default:
+            #warning("TODO")
+            break
+        }
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
