2f2f639e16c3fa9abc97edc42d159f2deb74dcd4
eliyap <yap@usc.edu>
2022-01-30 14:08:19 -0800

    draft a sendable replacement

--- Hyperthread/CardCell/Album/AlbumController.swift
+++ Hyperthread/CardCell/Album/AlbumController.swift
@@ -28,24 +28,24 @@ class AlbumController: UIPageViewController {
     /// The upcoming page number.
     private var pendingIndex: Int = 1
     
-    private static var countButtonConfig: UIButton.Configuration = {
-        var config = UIButton.Configuration.filled()
-        config.baseBackgroundColor = .systemGray6
-        config.cornerStyle = .capsule
-        config.titleTextAttributesTransformer = .init { incoming in
-            var outgoing = incoming
-            outgoing.foregroundColor = .label
-            outgoing.font = .preferredFont(forTextStyle: .footnote)
-            return outgoing
-        }
-        config.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
-        return config
-    }()
+//    private static var countButtonConfig: UIButton.Configuration = {
+//        var config = UIButton.Configuration.filled()
+//        config.baseBackgroundColor = .systemGray6
+//        config.cornerStyle = .capsule
+//        config.titleTextAttributesTransformer = .init { incoming in
+//            var outgoing = incoming
+//            outgoing.foregroundColor = .label
+//            outgoing.font = .preferredFont(forTextStyle: .footnote)
+//            return outgoing
+//        }
+//        config.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
+//        return config
+//    }()
     
     @MainActor
     init() {
-        self.countButton = UIButton(configuration: Self.countButtonConfig, primaryAction: nil)
-        
+//        self.countButton = UIButton(configuration: Self.countButtonConfig, primaryAction: nil)
+        self.countButton = CountButton()
         super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
         
         let superTall = view.heightAnchor.constraint(equalToConstant: .superTall)
@@ -64,7 +64,7 @@ class AlbumController: UIPageViewController {
             countButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -CardTeaserCell.borderInset),
         ])
         
-        countButton.adjustsImageSizeForAccessibilityContentSizeCategory = true
+//        countButton.adjustsImageSizeForAccessibilityContentSizeCategory = true
     }
     
     public func configure(tweet: Tweet) -> Void {
@@ -180,5 +180,28 @@ extension AlbumController: UIPageViewControllerDelegate {
         
         /// Update page number.
         countButton.setTitle("\(pendingIndex)/\(controllers.count)", for: .normal)
+    }
+}
+
+fileprivate final class CountButton: UIButton {
+    
+    private static var config: UIButton.Configuration = {
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .systemGray6
+        config.cornerStyle = .capsule
+        config.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
+        return config
+    }()
+    
+    @MainActor
+    init() {
+        #error("implement wrapper function that transforms attributed string as it comes in to replace non sendable transformer")
+        super.init(frame: .zero)
+        configuration = Self.config
+        adjustsImageSizeForAccessibilityContentSizeCategory = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
