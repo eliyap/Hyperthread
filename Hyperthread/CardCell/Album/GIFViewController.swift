9a08d90f573a9c699fa7c30b3cb14fa50553e91a
eliyap <yap@usc.edu>
2022-02-06 14:01:31 -0800

    draft GIF view

---
+++ Hyperthread/CardCell/Album/GIFViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GIFViewController.swift
+//  Hyperthread
+//
+//  Created by Secret Asian Man Dev on 6/2/22.
+//  Based on reference implementation at: https://developer.apple.com/documentation/avfoundation/avplayerlayer
+
+import UIKit
+import AVFoundation
+
+final class GIFView: UIView {
+    /// Override the property to make AVPlayerLayer the view's backing layer.
+    override static var layerClass: AnyClass { AVPlayerLayer.self }
+    
+    /// The associated player object.
+    var player: AVPlayer? {
+        get { playerLayer.player }
+        set { playerLayer.player = newValue }
+    }
+    
+    private var playerLayer: AVPlayerLayer { layer as! AVPlayerLayer }
+    
+    public init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
