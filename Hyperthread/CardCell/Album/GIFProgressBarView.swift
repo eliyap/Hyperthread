b3ad5fda6b65378f9511249c56f98c365312aaf4
eliyap <yap@usc.edu>
2022-02-13 13:41:45 -0800

    split file out

--- Hyperthread/CardCell/Album/GIFProgressBarView.swift
+++ Hyperthread/CardCell/Album/GIFProgressBarView.swift
@@ -85,7 +85,14 @@ final class ProgressVibrancyView: UIVisualEffectView {
     let label = UILabel()
     
     /// Solid template image subview in which the vibrancy effect is shown.
-    let solid = UIImageView.makeSolidTemplate()
+    /// Templated, resizable, solid image intended to fill a `UIVisualEffectView`.
+    /// This causes the vibrancy effect to fill the whole view.
+    ///
+    /// > `UIImageView` objects with images that have a rendering mode of `UIImage.RenderingMode.alwaysTemplate`
+    /// > as well as `UILabel` objects will update automatically.
+    ///
+    /// Source: https://developer.apple.com/documentation/uikit/uivibrancyeffect
+    let solid = UIImageView.makeSolidTemplate(color: .black)
     
     public var widthConstraint: NSLayoutConstraint? = nil
     
@@ -115,42 +122,5 @@ final class ProgressVibrancyView: UIVisualEffectView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-}
-
-/// Templated, resizable, solid image intended to fill a `UIVisualEffectView`.
-/// This causes the vibrancy effect to fill the whole view.
-///
-/// > `UIImageView` objects with images that have a rendering mode of `UIImage.RenderingMode.alwaysTemplate`
-/// > as well as `UILabel` objects will update automatically.
-///
-/// Source: https://developer.apple.com/documentation/uikit/uivibrancyeffect
-fileprivate extension UIImageView {
-    static func makeSolidTemplate() -> UIImageView {
-        let view = UIImageView()
-        guard let image = UIImage(color: UIColor.black)?.withRenderingMode(.alwaysTemplate) else {
-            Logger.general.error("Failed to render template image!")
-            assert(false)
-            return view
-        }
-        view.image = image
-        view.contentMode = .scaleToFill
-        return view
-    }
-}
-
-fileprivate extension UIImage {
-    /// Creates an image filled with solid color.
-    /// Source: https://stackoverflow.com/questions/26542035/create-uiimage-with-solid-color-in-swift
-    convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
-        let rect = CGRect(origin: .zero, size: size)
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
-        color.setFill()
-        UIRectFill(rect)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-    
-        guard let cgImage = image?.cgImage else { return nil }
-        self.init(cgImage: cgImage)
     }
 }
