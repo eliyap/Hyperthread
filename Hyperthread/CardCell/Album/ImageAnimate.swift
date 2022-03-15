bcf5d416ca6c1bc628e4bdeb8f0e2a3a620ef680
eliyap <yap@usc.edu>
2022-03-15 15:46:39 -0700

    recalculate frame on rotation

--- Hyperthread/CardCell/Album/ImageAnimate.swift
+++ Hyperthread/CardCell/Album/ImageAnimate.swift
@@ -33,6 +33,11 @@ final class LargeImageViewController: UIViewController {
         transitioningDelegate = self
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        view.frame = .init(origin: .zero, size: size)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
