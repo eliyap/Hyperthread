452cfab7f841b996cbe5c0386e7ad38761f45c90
eliyap <yap@usc.edu>
2022-03-16 17:32:42 -0700

    add KVO methods to controller

--- Hyperthread/CardCell/Album/VideoController.swift
+++ Hyperthread/CardCell/Album/VideoController.swift
@@ -10,8 +10,45 @@ import UIKit
 import AVKit
 
 final class VideoController: AVPlayerViewController {
+    
+    private let videoPlayer: AVPlayer = .init()
+    
+    /// Retains observation on playing video item.
+    private var videoObservation: NSKeyValueObservation? = nil
+    
     init() {
         super.init(nibName: nil, bundle: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.player = videoPlayer
+    }
+    
+    public func play(url: URL) -> Void {
+        /// Observe item, not player, as suggested in docs.
+        /// Docs: https://developer.apple.com/documentation/avfoundation/avplayer/1388096-status
+        let vidItem: AVPlayerItem = .init(url: url)
+        self.videoObservation = vidItem.observe(
+            \.status,
+             changeHandler: { [weak self] (object, change) in
+                 self?.respond(to: object, url: url)
+             })
+        
+        videoPlayer.replaceCurrentItem(with: vidItem)
+    }
+    
+    private func respond(to updatedItem: AVPlayerItem, url: URL) -> Void {
+        if updatedItem.status == .failed {
+            TableLog.error("""
+               Failed to play video!
+               - URL: \(url)
+               - Error: \(String(describing: updatedItem.error))
+               """)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: { [weak self] in
+                self?.play(url: url)
+            })
+        }
     }
     
     required init?(coder: NSCoder) {
