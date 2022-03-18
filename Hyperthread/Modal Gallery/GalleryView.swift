a9de30c85bef6ed05d99fa987d96b230ff442252
eliyap <yap@usc.edu>
2022-03-18 16:13:40 -0700

    split files

---
+++ Hyperthread/Modal Gallery/GalleryView.swift
@@ -0,0 +1,39 @@
+//
+//  GalleryView.swift
+//  Hyperthread
+//
+//  Created by Secret Asian Man Dev on 18/3/22.
+//
+
+import UIKit
+
+final class GalleryView: UIView {
+    
+    weak var pageView: ModalPageView!
+    
+    init(pageView: ModalPageView) {
+        self.pageView = pageView
+        super.init(frame: .zero)
+    }
+    
+    func constrain(to view: UIView) -> Void {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: view.topAnchor),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+        pageView.constrain(to: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension GalleryView: GeometryTargetProvider {
+    var targetView: UIView {
+        pageView.targetView
+    }
+}
--- Hyperthread/Modal Gallery/GalleryViewController.swift
+++ Hyperthread/Modal Gallery/GalleryViewController.swift
@@ -1,5 +1,5 @@
 //
-//  PageView.swift
+//  GalleryViewController.swift
 //  Hyperthread
 //
 //  Created by Secret Asian Man Dev on 17/3/22.
@@ -44,37 +44,6 @@ final class GalleryViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-}
-
-final class GalleryView: UIView {
-    
-    weak var pageView: ModalPageView!
-    
-    init(pageView: ModalPageView) {
-        self.pageView = pageView
-        super.init(frame: .zero)
-    }
-    
-    func constrain(to view: UIView) -> Void {
-        translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            topAnchor.constraint(equalTo: view.topAnchor),
-            bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        ])
-        pageView.constrain(to: self)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-extension GalleryView: GeometryTargetProvider {
-    var targetView: UIView {
-        pageView.targetView
     }
 }
 
