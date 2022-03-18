1fa59073e3cfc0c2e4904deb7cfe84f027b8af06
eliyap <yap@usc.edu>
2022-03-18 12:12:13 -0700

    declare controller

--- Hyperthread/CardCell/Album/PageView.swift
+++ Hyperthread/CardCell/Album/PageView.swift
@@ -118,6 +118,43 @@ final class ModalPageLayout: UICollectionViewFlowLayout {
     }
 }
 
+final class ModalPageViewController: UIViewController {
+    
+    private let pageView: ModalPageView
+    
+    public typealias Cell = ModalPageViewCell
+    
+    private let dataSource: ModalAlbumDataSource
+    
+    init(images: [UIImage?], rootView: UIView, startIndex: Int) {
+        let startIndex = IndexPath(item: startIndex, section: 0)
+        self.pageView = .init(rootView: rootView, image: images.first ?? nil, startIndex: startIndex)
+        self.dataSource = .init(collectionView: pageView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.reuseID, for: indexPath) as? Cell else {
+                fatalError("Failed to create or cast new cell!")
+            }
+            
+            let image: UIImage? = images[indexPath.item]
+            cell.configure(image: image, frame: collectionView.safeAreaLayoutGuide.layoutFrame)
+            
+            return cell
+        })
+        super.init(nibName: nil, bundle: nil)
+        
+        view = pageView
+        pageView.dataSource = dataSource
+        
+        var snapshot: ModalAlbumDataSource.Snapshot = .init()
+        snapshot.appendSections([.all])
+        snapshot.appendItems(Array(0..<images.count), toSection: .all)
+        dataSource.apply(snapshot)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 final class ModalPageView: UICollectionView {
     
     public weak var rootView: UIView?
