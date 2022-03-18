3ed3179bebea7a7760176c1f99ba29922462816f
eliyap <yap@usc.edu>
2022-03-18 16:25:51 -0700

    fix bad layout on rotating while zoomed in

--- Hyperthread/Modal Gallery/ModalPageViewController.swift
+++ Hyperthread/Modal Gallery/ModalPageViewController.swift
@@ -38,6 +38,20 @@ final class ModalPageViewController: UIViewController {
         dataSource.apply(snapshot)
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        /// On rotation, reset cell to 1x zoom, to prevent bad layout.
+        for cell in pageView.visibleCells {
+            guard let cell = cell as? ModalPageViewCell else {
+                assert(false, "Unexpected cell type \(type(of: cell))")
+                continue
+            }
+            cell.resetDisplay()
+            cell.willTransition(to: size)
+        }
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
