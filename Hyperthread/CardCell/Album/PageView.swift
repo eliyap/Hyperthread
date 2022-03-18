067f856260a3bc29cc2b456883bdfa7e212af816
eliyap <yap@usc.edu>
2022-03-18 12:01:20 -0700

    declare self invalidating layout

--- Hyperthread/CardCell/Album/PageView.swift
+++ Hyperthread/CardCell/Album/PageView.swift
@@ -64,6 +64,58 @@ final class ModalAlbumDataSource: UICollectionViewDiffableDataSource<AlbumSectio
     public typealias Snapshot = NSDiffableDataSourceSnapshot<AlbumSection, Int>
 }
 
+final class ModalPageLayout: UICollectionViewFlowLayout {
+    @MainActor
+    override init() {
+        super.init()
+        scrollDirection = .horizontal
+        
+        print("auto size\(self.estimatedItemSize == Self.automaticSize)")
+    }
+    
+    /// - Note: called when scrolling view, do not be over-zealous about invalidating.
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        if collectionView?.bounds.size != newBounds.size {
+            return true
+        } else {
+            return super.shouldInvalidateLayout(forBoundsChange: newBounds)
+        }
+    }
+    
+    /// Adapted from: https://medium.com/@ilabs/a-simple-guide-to-adjust-uicollectionview-to-device-rotation-8704093c2abe
+    override func invalidationContext(forBoundsChange newBounds: CGRect) -> UICollectionViewLayoutInvalidationContext {
+        let context = super.invalidationContext(forBoundsChange: newBounds)
+        guard
+            let context = context as? UICollectionViewFlowLayoutInvalidationContext,
+            let collectionView = self.collectionView
+        else { return context }
+        
+        let oldBounds = collectionView.bounds
+        
+        /// Request recalculation of cell sizes.
+        context.invalidateFlowLayoutDelegateMetrics = (oldBounds.size != newBounds.size)
+        
+        context.contentSizeAdjustment = CGSize(
+            width: (newBounds.size.width - oldBounds.size.width) * CGFloat(collectionView.numberOfItems(inSection: 0)),
+            height: newBounds.size.height - oldBounds.size.height
+        )
+        
+        for item in 0..<collectionView.numberOfItems(inSection: 0) {
+            guard let cell = collectionView.cellForItem(at: IndexPath(item: item, section: 0)) as? ModalPageViewCell else {
+                continue
+            }
+            cell.zoomableImageView.predictInsets(frame: newBounds)
+        }
+        
+        
+        return context
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 final class ModalPageView: UICollectionView {
     
     public weak var rootView: UIView?
