0a073574c467f31fa44d8e59cff28db666a825b1
eliyap <yap@usc.edu>
2022-03-27 15:20:03 -0700

    remove comments

--- Hyperthread/Modal Gallery/CTRef/CustomTextPosition.swift
+++ Hyperthread/Modal Gallery/CTRef/CustomTextPosition.swift
@@ -6,8 +6,6 @@ class CustomTextPosition: UITextPosition {
 	/// The offset from the start index of the text position
 	let offset: Int
 	
-	/// An initializer for a CustomTextPosition that takes in an offset
-	/// - Parameter offset: the offset from the start index of this text position
 	init(offset: Int) {
 		self.offset = offset
 	}
