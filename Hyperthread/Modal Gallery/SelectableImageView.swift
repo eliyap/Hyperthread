324f0b2b8883be0d627a8aebb03b83516b30c175
eliyap <yap@usc.edu>
2022-03-27 12:13:04 -0700

    clone init and delegates

--- Hyperthread/Modal Gallery/SelectableImageView.swift
+++ Hyperthread/Modal Gallery/SelectableImageView.swift
@@ -11,4 +11,25 @@ import BlackBox
 
 final class SelectableImageView: UIView {
     
+    private let visionImageView: VisionImageView
+    
+    /// Delegates.
+    public weak var imageVisionDelegate: ImageVisionDelegate? {
+        get { visionImageView.imageVisionDelegate }
+        set { visionImageView.imageVisionDelegate = newValue }
+    }
+    
+    public weak var textRequestDelegate: TextRequestDelegate? {
+        visionImageView
+    }
+    
+    @MainActor
+    init() {
+        self.visionImageView = .init()
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
