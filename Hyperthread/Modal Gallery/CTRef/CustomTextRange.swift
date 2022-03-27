347b5015458589cd952e3846a2d54a021a4e6e05
eliyap <yap@usc.edu>
2022-03-27 15:21:20 -0700

    remove comments

--- Hyperthread/Modal Gallery/CTRef/CustomTextRange.swift
+++ Hyperthread/Modal Gallery/CTRef/CustomTextRange.swift
@@ -1,27 +1,16 @@
-// Licensed under the MIT License.
-
 import UIKit
 
-/// A `UITextRange` subclass for use with `UITextInput`
 class CustomTextRange: UITextRange {
 	
-	/// The start offset of this range
 	let startOffset: Int
-	/// The end offset of this range
-	let endOffset: Int
 	
-	/// Create a `CustomTextRange` with the given offsets
-	/// - Parameters:
-	///   - startOffset: The start offset of this range
-	///   - endOffset: The end offset of this range
+    let endOffset: Int
+	
 	init(startOffset: Int, endOffset: Int) {
 		self.startOffset = startOffset
 		self.endOffset = endOffset
-		
 		super.init()
 	}
-	
-	// MARK: UITextRange Overrides
 	
 	override var isEmpty: Bool {
 		return startOffset == endOffset
@@ -34,5 +23,4 @@ class CustomTextRange: UITextRange {
 	override var end: UITextPosition {
 		return CustomTextPosition(offset: endOffset)
 	}
-	
 }
