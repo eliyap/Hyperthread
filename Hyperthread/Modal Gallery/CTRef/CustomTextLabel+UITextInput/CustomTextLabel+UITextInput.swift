cb07f636419b63a8638ea074b1d61b2275246603
eliyap <yap@usc.edu>
2022-03-27 15:10:58 -0700

    carve off unsupported functionality

--- Hyperthread/Modal Gallery/CTRef/CustomTextLabel+UITextInput/CustomTextLabel+UITextInput.swift
+++ Hyperthread/Modal Gallery/CTRef/CustomTextLabel+UITextInput/CustomTextLabel+UITextInput.swift
@@ -414,27 +414,4 @@ extension CustomTextLabel: UITextInput {
         // Turn our text position into an index into `labelText`
         return labelText.index(labelText.startIndex, offsetBy: max(position.offset, 0))
     }
-    
-    // MARK: - Writing Direction Conformance
-    func baseWritingDirection(for position: UITextPosition, in direction: UITextStorageDirection) -> NSWritingDirection {
-        return .leftToRight
-    }
-
-    func setBaseWritingDirection(_ writingDirection: NSWritingDirection, for range: UITextRange) {
-        assert(false, "Not Supported")
-    }
-
-    // MARK: - Edit Text Conformance
-    func insertText(_ text: String) { assert(false, "Not Supported") }
-    func deleteBackward() { assert(false, "Not Supported") }
-    func replace(_ range: UITextRange, withText text: String) { assert(false, "Not Supported") }
-
-    // MARK: - Marked Text Conformance
-    func setMarkedText(_ markedText: String?, selectedRange: NSRange) { assert(false, "Not Supported") }
-    func unmarkText() { assert(false, "Not Supported") }
-    var markedTextStyle: [NSAttributedString.Key : Any]? {
-        get { return nil }
-        set(markedTextStyle) { assert(false, "Not Supported") }
-    }
-    var markedTextRange: UITextRange? { return nil }
 }
