37b98c76d3cd2299f26df272f29f3a578a4059e2
eliyap <yap@usc.edu>
2022-03-28 07:16:37 -0700

    split file

--- Hyperthread/Modal Gallery/CTRef/CustomTextLabel.swift
+++ Hyperthread/Modal Gallery/CTRef/CustomTextLabel.swift
@@ -2,31 +2,6 @@
 
 import UIKit
 
-extension LiveString {
-    /// 2D index into a `LiveString`.
-    struct Index {
-        /// The "line" on which the index is located.
-        /// Min: 0
-        /// Max: lines.count - 1
-        let row: Int
-        
-        /// The "column" on which the index is located.
-        let column: String.Index
-        
-        public static let invalid: Self = .init(row: NSNotFound, column: "".startIndex)
-    }
-}
-
-extension LiveString.Index: Comparable {
-    static func <(lhs: Self, rhs: Self) -> Bool {
-		if (lhs.row == rhs.row) {
-			return lhs.column < rhs.column
-		} else { 
-			return lhs.row < rhs.row
-		}
-	}
-}
-
 /// A simple custom text label that conforms to `UITextInput` for use with `UITextInteraction`
 class CustomTextLabel: UIView {
 	
---
+++ Hyperthread/Modal Gallery/LiveText/LiveString.Index.swift
@@ -0,0 +1,33 @@
+//
+//  LiveString.Index.swift
+//  Hyperthread
+//
+//  Created by Secret Asian Man Dev on 28/3/22.
+//
+
+import Foundation
+
+extension LiveString {
+    /// 2D index into a `LiveString`.
+    struct Index {
+        /// The "line" on which the index is located.
+        /// Min: 0
+        /// Max: lines.count - 1
+        let row: Int
+        
+        /// The "column" on which the index is located.
+        let column: String.Index
+        
+        public static let invalid: Self = .init(row: NSNotFound, column: "".startIndex)
+    }
+}
+
+extension LiveString.Index: Comparable {
+    static func <(lhs: Self, rhs: Self) -> Bool {
+        if (lhs.row == rhs.row) {
+            return lhs.column < rhs.column
+        } else {
+            return lhs.row < rhs.row
+        }
+    }
+}
